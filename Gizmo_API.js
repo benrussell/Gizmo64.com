b6774b99b987a485d5d6e5718fa2770181fefac1
Ben Russell <br@x-plugins.com>
2014-04-04 20:11:36 +1100

    API Ref GUI: Adds code to highlight which leaf the user is currently reading using CSS styling.

--- Gizmo_API.js
+++ Gizmo_API.js
@@ -1,40 +1,61 @@
 
 
-function leafClick( branch, node_name ){
+function leafClick( leaf_div, branch, node_name ){
+
+	//alert( leaf_div );
+	//leaf_div.className = "leaf_active";
 
 	//alert( branch + "/" + node_name );
 
-	var leaves = gizmo_dox_data[branch];
 
+	var all_active_nav_leaves = document.getElementsByClassName("leaf_active");
+	for( var ileaf=0; ileaf < all_active_nav_leaves.length; ileaf++ ){
+		var target_leaf = all_active_nav_leaves[ ileaf ];
+		target_leaf.className = "leaf";
+	}
+
+	leaf_div.className = "leaf_active";
+
+
+
+	var leaves = gizmo_dox_data[branch];
 	var leaf = null;
 
 	var node_error = true; //this will catch nodes with no dox data.
-	//for( node in doxBranch ){
-	for( x=0; x<leaves.length; x++ ){
-	if( leaves[x][0] == node_name ){
-	//alert( leaves[x][1] );
-	leaf = leaves[x];
-	node_error = false;
-	}
+	for( var x=0; x<leaves.length; x++ ){
+		if( leaves[x][0] == node_name ){
+			//alert( leaves[x][1] );
+			leaf = leaves[x];
+			node_error = false;
 
-}
+			break;
+		} //names match?
 
-if( node_error ){
-	alert("Could not find the dox node!");
+	} //loop all data blocks looking for a name match
+
+
+
+	if( node_error ){
+		alert("Could not find the dox node!");
 	}else{
 
-	var div_header = document.getElementById("div_docBlock_Header");
-	var div_content = document.getElementById("div_docBlock_Content");
+		//This code controls updating the content in the RHS panel.
 
-	div_header.innerText = leaf[1];
-	div_content.innerHTML = "<pre>" + leaf[2] + "</pre>";
+		var div_header = document.getElementById("div_docBlock_Header");
+		var div_content = document.getElementById("div_docBlock_Content");
 
+		div_header.innerText = leaf[1];
+		div_content.innerHTML = "<pre>" + leaf[2] + "</pre>";
 
 	}
 
 return false;
 
 }//end leafClick( node )
+
+
+
+
 
 
 function buildSectionTree(){
@@ -53,7 +74,7 @@ function buildSectionTree(){
 	var leaves = gizmo_dox_data[key];
 	//alert(leaves.length);
 	for( x=0; x<leaves.length; x++ ){
-	var sNewLeaf = "<div id='leaf_"+leaves[x][0]+"' class='leaf' onclick='leafClick(\"" + key + "\", \"" + leaves[x][0] + "\");'>" + leaves[x][0] + "</div>";
+	var sNewLeaf = "<div id='leaf_"+leaves[x][0]+"' class='leaf' onclick='leafClick(this, \"" + key + "\", \"" + leaves[x][0] + "\");'>" + leaves[x][0] + "</div>";
 	divBlob += sNewLeaf;
 	}
 
@@ -119,10 +140,52 @@ function syncSectionTreeHeight(){
 function doSearch(){
 	var q = document.getElementById('txtQuery').value;
 
-	alert(q);
+	//alert(q);
+
+
+
+
+/*
+
+	var leaves = gizmo_dox_data[branch];
+	var leaf = null;
+
+	var node_error = true; //this will catch nodes with no dox data.
+	for( var x=0; x<leaves.length; x++ ){
+		if( leaves[x][0] == node_name ){
+			//alert( leaves[x][1] );
+			leaf = leaves[x];
+			node_error = false;
+
+			break;
+		} //names match?
+
+	} //loop all data blocks looking for a name match
+
+
+
+	if( node_error ){
+		alert("Could not find the dox node!");
+	}else{
+
+		//This code controls updating the content in the RHS panel.
+
+		var div_header = document.getElementById("div_docBlock_Header");
+		var div_content = document.getElementById("div_docBlock_Content");
+
+		div_header.innerText = leaf[1];
+		div_content.innerHTML = "<pre>" + leaf[2] + "</pre>";
+
+	}
+
+
+*/
+
+
+
 
 	document.getElementById('branch_leaves_debug').style.display = ''; //??
-	document.getElementById('branch_leaves_gfx').style.display = ''; // display hard coded parent branch opening as example of potential function
+	//document.getElementById('branch_leaves_gfx').style.display = ''; // display hard coded parent branch opening as example of potential function
 
 }
 
