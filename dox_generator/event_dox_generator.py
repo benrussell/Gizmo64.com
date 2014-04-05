19afb402faef92ca6f22e24649944b38ffe0c108
Ben Russell <br@x-plugins.com>
2014-04-05 20:07:19 +1100

    Lots of tweaks to event docs groupings.

--- dox_generator/event_dox_generator.py
+++ dox_generator/event_dox_generator.py
@@ -32,22 +32,24 @@ def save_data( filename, data ):
 known_event_docs = {}
 
 
-# --------- System Events -------------
+# --------- simtem Events -------------
 
 
 known_event_docs["OnSystemMenu_AboutGizmo"] = ["""
-function OnSystemMenu_AboutGizmo()
+<pre><code>function OnSystemMenu_AboutGizmo()
 --Gizmo64 XPL calls this when it wants to display the HotFix which is crafted from Lua widgets.
 end
-""","MENU"]
+</code></pre>
+""","menu"]
 
 
 
 known_event_docs["OnSystemMenu_HotFix"] = ["""
-function OnSystemMenu_AboutGizmo()
+<pre><code>function OnSystemMenu_HotFix()
 --Gizmo64 XPL calls this when it wants to display the About Dialog which is crafted from Lua widgets.
 end
-""","MENU"]
+</code></pre>
+""","menu"]
 
 
 
@@ -55,119 +57,149 @@ end
 
 
 known_event_docs["OnKickStart"] = ["""
-function OnKickStart()
+<pre><code>function OnKickStart()
 --Called during LuaVM restart. Used to initiate boot process. Detects which script to load for the Players current aircraft.
 end
-""","BOOT"]
+</code></pre>
+""","boot"]
 
 
 
 
 
 known_event_docs["OnError"] = ["""
-function OnError()
+<pre><code>function OnError()
 --Called when a LuaVM error is detected.
 --Allows interactive response to scripting errors. Default is connected to the Console extension.
 end
-""","LOG"]
+</code></pre>
+""","log"]
 
 known_event_docs["OnDebug"] = ["""
-function OnError()
---Called when a LuaVM error is detected.
+<pre><code>function OnDebug()
+--Called when a debug message is logged.
 --Allows interactive response to scripting errors. Default is connected to the Console extension.
 end
-""","LOG"]
+</code></pre>
+""","log"]
 
 known_event_docs["OnWarning"] = ["""
-function OnError()
---Called when a LuaVM error is detected.
+<pre><code>function OnWarning()
+--Called when a warning message is logged.
 --Allows interactive response to scripting errors. Default is connected to the Console extension.
 end
-""","LOG"]
+</code></pre>
+""","log"]
+
 
 
 
 
 
 known_event_docs["OnAircraftCrash"] = ["""
-function OnAircraftCrash()
+<pre><code>function OnAircraftCrash()
 --The user crashed the vehicle.
 end
-""","SYS"]
+</code></pre>
+""","sim"]
 
 
 known_event_docs["OnAircraftLoad"] = ["""
-function OnAircraftLoad()
+<pre><code>function OnAircraftLoad()
 --This function needs to accept an index number telling us which aircraft changed, currently it does not.
 --You know -an- aircraft was loaded, you don't know which one, it's kind of irrelevant anyway due to engine issues.(??? - br)
 end
-""","SYS"]
+</code></pre>
+""","sim"]
 
 
 known_event_docs["OnAircraftUnload"] = ["""
-function OnAicraftUnload()
+<pre><code>function OnAicraftUnload()
 --This function needs to accept an index number telling us which aircraft changed, currently it does not.
 --You know an aircraft was unloaded, but not which one.
 end
-""","SYS"]
+</code></pre>
+""","sim"]
 
 
 known_event_docs["OnAirportChanged"] = ["""
-function OnAirportChanged()
+<pre><code>function OnAirportChanged()
 --A new airport has been selected
 end
-""","SYS"]
+</code></pre>
+""","sim"]
+
+
+
+
 
 
 known_event_docs["OnBeforeReboot"] = ["""
-function OnBeforeReboot()
+<pre><code>function OnBeforeReboot()
 --This hook has been created to give you a chance to cleanup x-planes state.
 --This may include releasing overrides, restoring the autopilot/fmc, etc.
 end
-""","SYS"]
+</code></pre>
+""","boot"]
 
 
 known_event_docs["OnBoot"] = ["""
-function OnBoot()
+<pre><code>function OnBoot()
 --Gizmo has rebooted, init your code.
 --This event is also called when the user first loads your ACF file.
 end
-""","SYS"]
+</code></pre>
+""","boot"]
+
+
 
 
 known_event_docs["OnFirstFrame"] = ["""
-function OnFirstFrame()
+<pre><code>function OnFirstFrame()
 --OnBoot -> ACF Finishes Loading -> OnFirstFrame
 --set any "default acf state" datarefs here.
 end
+</code></pre>
 
 API: >= 11.5.12
-""","SYS"]
+""","boot"]
+
+
+
 
 
 known_event_docs["OnForceRedraw"] = ["""
-function OnForceRedraw()
+<pre><code>function OnForceRedraw()
 --The user has exited a full screen X-Plane dialog: Eg: WX/Joystick/Rendering options.
 --Does not work for: Load ACF / Change ACF Livery. Checked most others. Please report any that dont work.
-end
-
+end</code></pre>
 API: >= 11.5.12
-""","SYS"]
+""","sim"]
+
+
+
 
 
 known_event_docs["OnLiveryChanged"] = ["""
- function OnLiveryChanged()
+ <pre><code>function OnLiveryChanged()
  
  --The user has changed their livery, check acf.getLiveryName() for more info.
  
  end
+ </code></pre>
  
  API >= 11.9.1
- ""","SYS"]
+ ""","sim"]
+
+
+
+
+
+
 
 
 known_event_docs["OnMouseClick"] = ["""
-function OnMouseClick()
+<pre><code>function OnMouseClick()
 --mouse.click.x, mouse.click.y and mouse.click.e 
 --are automatically updated by Gizmo before this function is called.
 --You may use them immediately.
@@ -177,31 +209,39 @@ function OnMouseClick()
 
 --See the mouse.api module documentation for more details.
 end
-""","SYS"]
+</code></pre>
+""","input"]
 
 
 known_event_docs["OnMouseWheel"] = ["""
- function OnMouseWheel( x,y, wheel,clicks )
+ <pre><code>function OnMouseWheel( x,y, wheel,clicks )
 	--x,y in screen coordinates
 	--wheel and clicks are int values, unkown details.
  
  end
+ </code></pre>
  
- ""","SYS"]
+ ""","input"]
+
+
+
+
+
+
 
 
 known_event_docs["OnPluginDisable"] = ["""
 function OnPluginDisable()
 --The plugin has been disabled.
 end
-""","SYS"]
+""","plugin"]
 
 
 known_event_docs["OnPluginEnable"] = ["""
 function OnPluginEnable()
 --The plugin has been enabled.
 end
-""","SYS"]
+""","plugin"]
 
 
 known_event_docs["OnPluginMessage"] = ["""
@@ -217,7 +257,7 @@ known_event_docs["OnPluginMessage"] = ["""
  --if we do not return any value Gizmo will continue looking for an event handler for this plugin message type.
  
  end
- ""","SYS"]
+ ""","plugin"]
 
 
 known_event_docs["OnPluginMessageEx"] = ["""
@@ -235,35 +275,52 @@ _Events.OnPluginMessageEx( sig, path, message, integer_param )
 	--if we do not return any value Gizmo will continue looking for an event handler for this plugin message type.
  
  end
-""","SYS"]
+""","plugin"]
 
- 
+
+
+
+
+
+
+
+
 known_event_docs["OnSceneryLoaded"] = ["""
 function OnSceneryLoaded()
 --New scenery tiles were loaded
 end
-""","SYS"]
+""","sim"]
+
+
+
+
+
+
 
 
 known_event_docs["OnScriptError"] = ["""
 function OnScriptError( message_string )
 --A script error has been thrown.
 end
-""","SYS"]
+""","lua"]
 
 
 known_event_docs["OnScriptWarning"] = ["""
 function OnScriptWarning( message_string )
 --A script warning has been thrown.
 end
-""","SYS"]
+""","lua"]
+
+
+
+
 
 
 known_event_docs["OnSituationLoaded"] = ["""
 function OnSituationLoaded()
 --A .sit file completed loading.
 end
-""","SYS"]
+""","sim"]
 
 
 known_event_docs["OnTrafficCountChanged"] = ["""
@@ -272,14 +329,17 @@ known_event_docs["OnTrafficCountChanged"] = ["""
 	--The number of traffic aircraft has been changed
  
  end
- ""","SYS"]
+ ""","sim"]
+
+
+
 
 
 known_event_docs["OnUpdate"] = ["""
 function OnUpdate()
 --this is your basic "every frame" function call, do most of your work here.
 end
-""","SYS"]
+""","sim"]
 
 
 
@@ -293,45 +353,47 @@ end
 
 known_event_docs["OnDraw_BeforeGauges"] = ["""
 function OnDraw_BeforeGauges()
-
+	--Using this function can be more suitable for mixing Avionics with X-Planes built in items.
+	--Combined with the next function you should be able to achieve most tasks.
 end
- ""","2D"]
+ ""","GFX 2D"]
 
 known_event_docs["OnDraw_BeforeGauges_3D"] = ["""
 function OnDraw_BeforeGauges_3D()
-
+--3d Panel Phase, gauge background, bezels etc
+--The function names have 3D in them but the drawing modes are purely 2D GL.
 end
- ""","2D"]
+""","GFX 2D"]
 
 known_event_docs["OnDraw_BeforeLastCockpit"] = ["""
 function OnDraw_BeforeLastCockpit()
 
 end
- ""","2D"]
+ ""","GFX 2D"]
 
 known_event_docs["OnDraw_BeforeLastScene"] = ["""
 function OnDraw_BeforeLastScene()
 
 end
- ""","2D"]
+ ""","GFX 2D"]
 
 known_event_docs["OnDraw_BeforeLocalMap2D"] = ["""
 function OnDraw_BeforeLocalMap2D()
 
 end
- ""","2D"]
+ ""","GFX 2D"]
 
 known_event_docs["OnDraw_BeforeLocalMapProfile"] = ["""
 function OnDraw_BeforeLocalMapProfile()
 
 end
- ""","2D"]
+ ""","GFX 2D"]
 
 known_event_docs["OnDraw_BeforeWindows"] = ["""
 function OnDraw_BeforeWindows()
 
 end
- ""","3D"]
+ ""","GFX 2D"]
 
 
 
@@ -340,39 +402,34 @@ known_event_docs["OnDraw_FirstCockpit"] = ["""
 	end
 	function OnDraw_FirstCockpit()
 	end
- ""","2D"]
+ ""","GFX 2D"]
+
+
+
 
 
 known_event_docs["OnDraw_Gauges"] = ["""
-	function OnDraw_BeforeGauges()
-		--Using this function can be more suitable for mixing Avionics with X-Planes built in items.
-		--Combined with the next function you should be able to achieve most tasks.
-	end
-	function OnDraw_Gauges()
-		--You should draw your Panel Gauges / Avionics displays in this function.
-	end
- ""","2D"]
+function OnDraw_Gauges()
+	--You should draw your Panel Gauges / Avionics displays in this function.
+end
+""","GFX 2D"]
 
 
 known_event_docs["OnDraw_Gauges_3D"] = ["""
-	function OnDraw_BeforeGauges_3D()
-		--3d Panel Phase, gauge background, bezels etc
-		--The function names have 3D in them but the drawing modes are purely 2D GL.
-	end
-	function OnDraw_Gauges_3D()
-		--3D panel phase
-	end
- ""","2D"]
+function OnDraw_Gauges_3D()
+	--3D panel phase
+end
+ ""","GFX 2D"]
 
 
 known_event_docs["OnDraw_LastCockpit"] = ["""
-	function OnDraw_BeforeLastCockpit()
+function OnDraw_BeforeLastCockpit()
 
-	end
-	function OnDraw_LastCockpit()
+end
+function OnDraw_LastCockpit()
 
-	end
-""","2D"]
+end
+""","GFX 2D"]
 
 
 known_event_docs["OnDraw_Panel"] = ["""
@@ -381,42 +438,52 @@ known_event_docs["OnDraw_Panel"] = ["""
  --end
  --function OnDraw_Panel()
  --end
- ""","2D"]
+ ""","GFX 2D"]
 
 
 known_event_docs["OnDraw_LocalMap2D"] = ["""
-	--WARNING: These functions have ambiguous names that will be changed.
-	--They have been added "for completeness" and don't fit in with the Gizmo naming conventions as they are.
-	function OnDraw_BeforeLocalMap2D()
+--WARNING: These functions have ambiguous names that will be changed.
+--They have been added "for completeness" and don't fit in with the Gizmo naming conventions as they are.
+function OnDraw_BeforeLocalMap2D()
 
-	end
-	function OnDraw_LocalMap2D()
+end
+function OnDraw_LocalMap2D()
 
-	end
- ""","2D"]
+end
+ ""","GFX 2D"]
 
 
 known_event_docs["OnDraw_LocalMapProfile"] = ["""
-	function OnDraw_BeforeLocalMapProfile()
+function OnDraw_BeforeLocalMapProfile()
 
-	end
-	function OnDraw_LocalMapProfile()
+end
+function OnDraw_LocalMapProfile()
 
-	end
- ""","2D"]
+end
+ ""","GFX 2D"]
+
+
+
+
+
 
 
 known_event_docs["OnDraw_Windows"] = ["""
-	function OnDraw_BeforeWindows()
+function OnDraw_BeforeWindows()
 
-	--Draw your own 2d popups here
-	end
-	function OnDraw_Windows()
+--Draw your own 2d popups here
+end
+function OnDraw_Windows()
 
-	--Draw your own 2d popups here
-	end
-""","2D"]
+--Draw your own 2d popups here
+end
+""","GFX 2D"]
 
+known_event_docs["OnDraw_BeforeFirstCockpit"] = ["""
+function OnDraw_BeforeFirstCockpit()
+
+end
+ ""","GFX 2D"]
 
 
 
@@ -429,39 +496,34 @@ known_event_docs["OnDraw_BeforeFirstScene"] = ["""
 function OnDraw_BeforeFirstScene()
 
 end
- ""","3D"]
+ ""","GFX 3D"]
 
 known_event_docs["OnDraw_BeforeAirplanes"] = ["""
 function OnDraw_BeforeAirplanes()
 
 end
- ""","3D"]
+ ""","GFX 3D"]
 
 known_event_docs["OnDraw_BeforeAirports"] = ["""
 function OnDraw_BeforeAirports()
 
 end
- ""","3D"]
+ ""","GFX 3D"]
 
 
-known_event_docs["OnDraw_BeforeFirstCockpit"] = ["""
-function OnDraw_BeforeFirstCockpit()
-
-end
- ""","3D"]
 
 
 known_event_docs["OnDraw_BeforeTerrain"] = ["""
 function OnDraw_BeforeTerrain()
 
 end
- ""","3D"]
+ ""","GFX 3D"]
 
 known_event_docs["OnDraw_BeforeVectors"] = ["""
 function OnDraw_BeforeVectors()
 
 end
- ""","3D"]
+ ""","GFX 3D"]
 
 
 
@@ -471,72 +533,72 @@ known_event_docs["OnDraw_FirstScene"] = ["""
 function OnDraw_FirstScene()
 
 end
- ""","3D"]
+ ""","GFX 3D"]
 
 
 known_event_docs["OnDraw_Terrain"] = ["""
 	function OnDraw_Terrain()
 	
 	end
-""","3D"]
+""","GFX 3D"]
 
 
 known_event_docs["OnDraw_Airports"] = ["""
 	function OnDraw_Airports()
 	
 	end
- ""","3D"]
+ ""","GFX 3D"]
 
 
 known_event_docs["OnDraw_Vectors"] = ["""
 	function OnDraw_Vectors()
 	
 	end
-""","3D"]
+""","GFX 3D"]
 
 
 known_event_docs["OnDraw_World"] = ["""
-	function OnDraw_World()
-	
-	--Draw objects in the world next to your aircraft, your aircraft will be drawn after them so effects should integrate well
-	--Called during: xplm_Phase_Objects
-	end
- ""","3D"]
+function OnDraw_World()
+
+--Draw objects in the world next to your aircraft, your aircraft will be drawn after them so effects should integrate well
+--Called during: xplm_Phase_Objects
+end
+ ""","GFX 3D"]
 
 
 
 
 
 known_event_docs["OnDraw_DebugFlightPath"] = ["""
-	function OnDraw_DebugFlightPath()
-	--Use this to draw graphics when x-planes purple flight path is turned on
-	--The actual SDK phase is currently xplm_Phase_Objects
-	end
- ""","3D"]
+function OnDraw_DebugFlightPath()
+--Use this to draw graphics when x-planes purple flight path is turned on
+--The actual SDK phase is currently xplm_Phase_Objects
+end
+ ""","GFX 3D"]
 
 
 known_event_docs["OnDraw_Airplanes"] = ["""
-	function OnDraw_Airplanes()
-	
-	end
- ""","3D"]
+function OnDraw_Airplanes()
+
+end
+ ""","GFX 3D"]
 
 
 known_event_docs["OnDraw_LastScene"] = ["""
-	function OnDraw_LastScene()
-	
-	end
-""","3D"]
+function OnDraw_LastScene()
+
+end
+""","GFX 3D"]
 
 
 known_event_docs["OnDraw_LocalMap3D"] = ["""
 --WARNING: This function needs a name tweak to match the panel drawing signature convetion.
 
-	function OnDraw_LocalMap3D()
-	
-	end
+function OnDraw_LocalMap3D()
 
-""","3D"]
+end
+
+""","GFX 3D"]
 
 
 
@@ -594,12 +656,23 @@ if( os.path.exists(input_folder) and os.path.isdir(input_folder) ):
 					
 						if( event_name in events_list ):
 							events_list[ event_name ] += 1
-							print( "%s <-- duplicate" %(event_name) )
+							#print( "%s <-- duplicate" %(event_name) )
 							
 						else:
 							events_list[ event_name ] = 1
 					
-					
+					m = re.match(".*\"(_On.*)\".*$", l)
+					if( m ):
+						event_name = str(m.groups(1)[0])
+						match_count += 1
+
+						if( event_name in events_list ):
+							events_list[ event_name ] += 1
+							#print( "%s <-- duplicate" %(event_name) )
+
+						else:
+							events_list[ event_name ] = 1
+
 	# end looping all input files...
 
 
@@ -624,7 +697,7 @@ if( os.path.exists(input_folder) and os.path.isdir(input_folder) ):
 # 	if( k in known_event_docs ):
 # 		pass
 # 		#print( "\"%s\"," %(k) ) #Event name
-# 		#print(known_event_docs[ k ][1]) #output subsystem that owns this event
+# 		#print(known_event_docs[ k ][1]) #output subsimtem that owns this event
 # 		#print(known_event_docs[ k ][0]) #output description data
 # 	else:
 # 		print( "\"%s\"," %(k) ) #event name
@@ -653,47 +726,6 @@ api_data = []
 
 
 
-## This code is used to decode the script data and re-assemble it in a way thats useful for JSON output.
-# for sig in sorted(list_of_api_function_data):
-# 	if( list_of_api_function_data[sig] == "" ):
-# 		#print("Missing dox: %s" %(sig))
-# 		warnings.append( "Missing dox: %s" %(sig) )
-#
-# 	#print(sig)
-# 	#print( list_of_api_function_data[sig] )
-#
-# 	m = re.match("^(.+)\(.*", sig)
-# 	clean_sig = sig
-#
-# 	json_sig = string.strip(sig)
-# 	json_dox = (list_of_api_function_data[sig])
-# 	json_subsys = "hardcoded"
-#
-# 	if( m ):
-# 		clean_sig = str(m.groups(1)[0])
-# 		json_sig = clean_sig
-# 		print( sig )
-# 		clean_parent = clean_sig.split(".")[0]
-# 		if( clean_parent != last_clean_p ):
-# 			last_clean_p = clean_parent
-# 			#print("foo")
-#
-# 			if( clean_parent in api_sections ):
-# 				 pass
-# 				#print("ignore")
-# 			else:
-# 				api_sections[clean_parent] = []
-# 				print("\n**** created storage for: %s" %(last_clean_p))
-#
-# 			#api_sections.append( clean_parent )
-#
-# 	#print( "key: (%s)" %(last_clean_p) )
-# 	#print( "sig: (%s)" %(json_sig) )
-# 	#print( "key: (%s)" %(last_clean_p) )
-# 	if( last_clean_p != "" ):
-# 		api_sections[last_clean_p].append( [json_sig, sig, json_dox] )
-
-
 
 
 print("")
@@ -702,19 +734,19 @@ for k in sorted(events_list):
 	if( k in known_event_docs ):
 		event_name = k
 		description = known_event_docs[ k ][0]
-		subsys_name = known_event_docs[ k ][1]
+		subsim_name = known_event_docs[ k ][1]
 
 		#ensure that we have dict storage for this data.
-		if( subsys_name in api_sections ):
+		if( subsim_name in api_sections ):
 			pass
 		else:
-			api_sections[ subsys_name ] = []
+			api_sections[ subsim_name ] = []
 
-		api_sections[ subsys_name ].append( [ event_name, event_name, description, subsys_name ] )
+		api_sections[ subsim_name ].append( [ event_name, event_name, description, subsim_name ] )
 
 	else:
 		print( "\"%s\"," %(k) ) #event name
-		print("\t NO DOX: %s ***" %(k))
+		#print("\t NO DOX: %s ***" %(k))
 
 print("")
 print( match_count )
@@ -742,9 +774,9 @@ for k in sorted(set(api_sections)):
 		json_sig = string.replace( json_sig, "'", "\\'")
 
 
-		json_subsys = node[3]
+		json_subsim = node[3]
 		
-		json_blob += "\t\t['%s', '%s', '%s', '%s']" %(node[0], json_sig, json_dox, json_subsys)
+		json_blob += "\t\t['%s', '%s', '%s', '%s']" %(node[0], json_sig, json_dox, json_subsim)
 		
 		node_x += 1
 		if( node_x < max_x ):
