7449d9a9603309bc95f442d43433e5accd44799d
Ben Russell <br@x-plugins.com>
2014-09-11 02:08:10 +1000

    Updated cmd API docs. Autogen stubs now contain generation date.

--- autocomplete stubs/autocomplete stubs/IntelliJ/cmd.lua
+++ autocomplete stubs/autocomplete stubs/IntelliJ/cmd.lua
@@ -1,6 +1,6 @@
 --[[
 Gizmo-Docs Lua Stub
-Generated: __date__ __time__
+Generated: 2014/09/11 02:07:07
 
 Created by: https://github.com/benrussell/Gizmo-Docs-Generator
 --]]
@@ -9,5 +9,7 @@ module "cmd"
 
 function beginByName( command_name ) end
 function endByName( command_name ) end
+function getOnHoldDelay( cmd_id ) end
 function newCommand( command_name, description, script_function_name ) end
 function onceByName( command_name ) end
+function setOnHoldDelay( cmd_id, new_delay ) end
--- autocomplete stubs/autocomplete stubs/IntelliJ/trimesh.lua
+++ autocomplete stubs/autocomplete stubs/IntelliJ/trimesh.lua
@@ -1,6 +1,6 @@
 --[[
 Gizmo-Docs Lua Stub
-Generated: __date__ __time__
+Generated: 2014/09/11 02:07:07
 
 Created by: https://github.com/benrussell/Gizmo-Docs-Generator
 --]]
