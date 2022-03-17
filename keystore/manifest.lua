71d614707b57410450fff2471e38f2ed749d0b46
Ben Russell <br@x-plugins.com>
2022-03-17 15:13:11 +1100

    Created keystore manifest

---
+++ keystore/manifest.lua
@@ -0,0 +1,12 @@
+{
+    version = 2203170153,
+    keys = {
+        "Digital Replica",
+        "ITXD",
+        "IXEG",
+        "Leading Edge Simulations",
+        "Uncle Jack Simulations",
+        "x-aviation.com",
+        "X-Scenery",        
+    }
+}
